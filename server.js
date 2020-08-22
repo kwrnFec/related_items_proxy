0b3750bfd95a871f3313bf8fa002e606f0d5891d
richard-d-lee <richdelee@gmail.com>
2020-08-22 10:05:35 -0600

    Updated Proxy Server

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-//hello
+const express = require("express");
+const axios = require("axios");
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const port = 3001;
+
+// const rpUrl = 'http://ec2-13-58-209-140.us-east-2.compute.amazonaws.com/';
+const rpUrl = 'http://localhost:3000/';
+
+
+let app = express();
+
+app.use(express.static('public'));
+
+app.use('/rp', createProxyMiddleware({ target: rpUrl, changeOrigin: true }));
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
